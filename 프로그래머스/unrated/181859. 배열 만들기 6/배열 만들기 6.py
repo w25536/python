3f48e0694db1f37b6ea0f1bf4196fc74a5c6e543
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2023-08-09 14:49:57 +0900

    [unrated] Title: 배열 만들기 6, Time: 9.49 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -16,3 +16,8 @@ def solution(arr):
 #Pop - Remove and return the top element from the stack.
 #Peek/Top - View the top element without removing it.
 #isEmpty - Check if the stack is empty.
+
+#Push is equivalent to the list's append() method.
+#Pop is the list's pop() method.
+#Peek/Top can be achieved by accessing the last element, i.e., stk[-1].
+#isEmpty is as simple as checking if not stk:.
