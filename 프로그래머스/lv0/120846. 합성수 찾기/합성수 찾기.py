c462752a13a9621106fd50b9c53409dc8d690d32
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2023-08-21 16:18:30 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -6,4 +6,4 @@ def divisor(n):
     return divisor_list
 
 def solution(n):
-    return len([num for num in range(1,n+1) if len(divisor(num)) >= 3]) 
+    return len([num for num in range(1,n+1) if len([i for i in range(1, n+1) if num % i == 0]) >= 3]) 
