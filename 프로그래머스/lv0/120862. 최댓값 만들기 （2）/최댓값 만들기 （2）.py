cde97ebbc3fdaba66bb038c0c0f063708419f220
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2023-08-21 10:23:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,3 +1,3 @@
 def solution(numbers):
-	return sorted(numbers)[len(numbers)-1] * sorted(numbers)[len(numbers)-2] if sorted(numbers)[len(numbers)-1] * sorted(numbers)[len(numbers)-2] > sorted(numbers)[0] * sorted(numbers)[1] else sorted(numbers)[0] * sorted(numbers)[1]
-    
+    return max(sorted(numbers)[0] * sorted(numbers)[1], sorted(numbers)[len(numbers)-1] * sorted(numbers)[len(numbers)-2])
+  
