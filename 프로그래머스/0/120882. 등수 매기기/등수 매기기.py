1a3e1d6d40635d30065b785973ca0c97b7d197ce
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2024-02-15 22:39:23 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,20 +1,7 @@
 def solution(score):
-    answer = []
-
-    averages = [(s[0] + s[1]) / 2 for s in score]
-
-    indexed_averages = list(enumerate(averages))
-
-    sorted_averages = sorted(indexed_averages, key=lambda x: -x[1])
     
-    ranks = [0] * len(score)
-    current_rank = 1
-    for i, (index, _) in enumerate(sorted_averages):
-        if i > 0 and sorted_averages[i][1] == sorted_averages[i - 1][1]:
-            
-            ranks[sorted_averages[i][0]] = ranks[sorted_averages[i-1][0]]
-        else:
-            ranks[index] = current_rank
-        current_rank += 1
+    a = sorted([sum(i) for i in score], reverse = True)
     
-    return ranks
+    print(a)
+    
+    return [a.index(sum(i)) + 1 for i in score]
